b48f195ccaeb1e24cf3453d7c8cb946f9793a275
CodeLegg <michaelraymondlegg@gmail.com>
2023-12-03 22:36:35 +0000

    Refactor flipCard function and add match checking logic

--- assets/js/game-script.js
+++ assets/js/game-script.js
@@ -1,8 +1,45 @@
+const cards = document.querySelectorAll(".memory-card");
 
-const cards = document.querySelectorAll('.memory-card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
 function flipCard() {
-  this.classList.toggle('flip');
+  this.classList.add("flip");
+
+  if (!hasFlippedCard) {
+    // first click
+    hasFlippedCard = true;
+    firstCard = this;
+  } else {
+    // second click
+    hasFlippedCard = false;
+    secondCard = this;
+
+    checkForMatch();
+  }
 }
 
-cards.forEach(card => card.addEventListener('click', flipCard));
+function checkForMatch() {
+  // do cards match?
+  if (firstCard.dataset.framework === secondCard.dataset.framework) {
+    // it's a match!
+    disableCards();
+  } else {
+    // not a match
+    unflipCards();
+  }
+}
+
+function disableCards() {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+}
+
+function unflipCards() {
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+  }, 1500);
+}
+
+cards.forEach((card) => card.addEventListener("click", flipCard));
