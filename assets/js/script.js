41762e08a6c39f8e8d87bf442d9c766e6a0ebe34
CodeLegg <michaelraymondlegg@gmail.com>
2023-12-03 22:03:30 +0000

    Add fade-in animation to start button

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const startButton = document.getElementById("start-button");
+
+// Add a class for initial styles (opacity: 0, pointer-events: none)
+startButton.classList.add("hidden");
+
+// After a delay of 8000 milliseconds (8 seconds)
+setTimeout(function () {
+    // Remove the class to update styles (opacity: 1, pointer-events: auto)
+    startButton.classList.remove("hidden");
+    // Add a class for initial styles (opacity: 1, transition: opacity 2s ease;)
+    startButton.classList.add("fade-in");
+  }, 8000);
