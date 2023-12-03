68c22300c5f43c3762042960f6b5e03de237e114
CodeLegg <michaelraymondlegg@gmail.com>
2023-12-03 22:47:24 +0000

    Fix flipCard function and add resetBoard function

--- assets/js/game-script.js
+++ assets/js/game-script.js
@@ -5,7 +5,8 @@ let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
-if (lockBoard) return;
+  if (lockBoard) return;
+  if (this === firstCard) return;
 
   this.classList.add("flip");
 
@@ -37,10 +38,19 @@ function disableCards() {
 }
 
 function unflipCards() {
+  lockBoard = true;
+
   setTimeout(() => {
     firstCard.classList.remove("flip");
     secondCard.classList.remove("flip");
+
+    resetBoard();
   }, 1500);
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
 }
 
 cards.forEach((card) => card.addEventListener("click", flipCard));
