231af09fc76fa6f45b5030fcc8043262a4df13f3
CodeLegg <michaelraymondlegg@gmail.com>
2023-12-04 16:44:28 +0000

    Add shuffle functionality to game script

--- assets/js/game-script.js
+++ assets/js/game-script.js
@@ -73,6 +73,13 @@ function resetBoard() {
   [firstCard, secondCard] = [null, null];
 }
 
+(function shuffle() {
+  cards.forEach((card) => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
+
 cards.forEach((card) => card.addEventListener("click", flipCard));
 
 let timer;
